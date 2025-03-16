c899b1cdfa940259854e3c10b899d3e79bb91786
Rajat Sharma <sharmarajat687@gmail.com>
2025-03-16 20:18:48 +0530

    re-arrange folders

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cron = require("node-cron");
 require("dotenv").config();
-const db = require("./database");
+const db = require("./config/database");
 const app = express();
 const router = require("./routes/Subscribe")
 db
